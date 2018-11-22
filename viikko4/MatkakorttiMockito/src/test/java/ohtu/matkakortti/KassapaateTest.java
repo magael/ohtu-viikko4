1cddcb0ca0b8a63823b8f510efaa423c9342ce22
Mikael Jaakkola <mikael.jaakkola@helsinki.fi>
2018-11-22 23:51:41 +0200

    tehtävä 3

--- viikko4/MatkakorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
+++ viikko4/MatkakorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
@@ -1,4 +1,3 @@
-
 package ohtu.matkakortti;
 
 import ohtu.matkakortti.Matkakortti;
@@ -13,21 +12,21 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class KassapaateTest {
-    
+
     Kassapaate kassa;
     Matkakortti kortti;
-    
+
     @Before
     public void setUp() {
         kassa = new Kassapaate();
         kortti = mock(Matkakortti.class);
     }
-    
+
     @Test
     public void kortiltaVelotetaanHintaJosRahaaOn() {
         when(kortti.getSaldo()).thenReturn(10);
         kassa.ostaLounas(kortti);
-        
+
         verify(kortti, times(1)).getSaldo();
         verify(kortti).osta(eq(Kassapaate.HINTA));
     }
@@ -36,9 +35,22 @@ public class KassapaateTest {
     public void kortiltaEiVelotetaJosRahaEiRiita() {
         when(kortti.getSaldo()).thenReturn(4);
         kassa.ostaLounas(kortti);
-        
+
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
-      
+
+    @Test
+    public void kortilleLadataanSaldoaJosSummaOnPositiivinen() {
+        kassa.lataa(kortti, 1);
+
+        verify(kortti, times(1)).lataa(1);
+    }
+
+    @Test
+    public void kortilleEiLadataSaldoaJosSummaOnNegatiivinen() {
+        kassa.lataa(kortti, -1);
+
+        verify(kortti, times(0)).lataa(-1);
+    }
 }
